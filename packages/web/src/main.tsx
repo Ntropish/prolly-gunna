e961d82e14461fb2f52071ae79f874fa0990df17
Justin Stone <ntropish@gmail.com>
2025-05-31 21:32:00 -0500

    Add ThemeProvider to main.tsx for improved theming support

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -8,6 +8,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "./components/app-sidebar.tsx";
+import { ThemeProvider } from "./components/theme-provider.tsx";
 
 const queryClient = new QueryClient();
 
@@ -17,16 +18,18 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <SidebarProvider>
-          <AppSidebar />
-          <main className="flex flex-col gap-4 overflow-y-hidden flex-1 relative">
-            <SidebarTrigger className="absolute top-2 left-2" />
-            <Routes>
-              <Route index element={<App />} />
-              <Route path="/:treeId" element={<App />} />
-            </Routes>
-          </main>
-        </SidebarProvider>
+        <ThemeProvider>
+          <SidebarProvider>
+            <AppSidebar />
+            <main className="flex flex-col gap-4 overflow-y-hidden flex-1 relative">
+              <SidebarTrigger className="absolute top-2 left-2" />
+              <Routes>
+                <Route index element={<App />} />
+                <Route path="/:treeId" element={<App />} />
+              </Routes>
+            </main>
+          </SidebarProvider>
+        </ThemeProvider>
       </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
