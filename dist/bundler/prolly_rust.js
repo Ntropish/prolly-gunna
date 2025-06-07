f48e7468a7ff48cfde8bcc483cd9e42b8e7485fd
Justin Stone <ntropish@gmail.com>
2025-06-07 13:09:46 -0500

    Add bundler dist

---
+++ dist/bundler/prolly_rust.js
@@ -0,0 +1,5 @@
+import * as wasm from "./prolly_rust_bg.wasm";
+export * from "./prolly_rust_bg.js";
+import { __wbg_set_wasm } from "./prolly_rust_bg.js";
+__wbg_set_wasm(wasm);
+wasm.__wbindgen_start();
